42d1b7d83de526424306a58a2a0ed46d9cba9fd5
Praveen Saravanan <praveen747757@gmail.com>
2026-03-12 23:40:42 +0530

    feat: add author information to metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,12 @@ export const metadata: Metadata = {
     'The Rotary Club of Pudukkottai Metro is a newly chartered Rotary International club (District 3000, Club ID 227596) in Pudukkottai, Tamil Nadu, dedicated to service, fellowship and community development.',
   keywords:
     'Rotary Club Pudukkottai Metro, Rotary District 3000, Service Above Self, Rotary International India',
+  authors: [
+    {
+      name: 'Praveen Saravanan',
+      url: 'https://www.linkedin.com/in/praveen-saravanan-99688b128',
+    },
+  ],
 };
 
 export default function RootLayout({
